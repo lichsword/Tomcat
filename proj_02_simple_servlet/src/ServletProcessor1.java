8996f21da88c101596ee1db33d5049568f3aac70
简悦 <wangyue.wy@taobao.com>
2014-03-04 17:04:21 +0800

    "引入servlet包，完成简单的servlet服务器，支持静态文件和Servlet容器"

---
+++ proj_02_simple_servlet/src/ServletProcessor1.java
@@ -0,0 +1,58 @@
+import javax.servlet.Servlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLStreamHandler;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lichsword
+ * Date: 14-3-3
+ * Time: 下午9:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServletProcessor1 {
+
+    public ServletProcessor1() {
+    }
+
+    public void process(Request request, Response response) {
+        String uri = request.getUri();
+        String servletName = uri.substring(uri.lastIndexOf("/") + 1);
+        URLClassLoader loader = null;
+        try {
+            // 创建一个 Class loader
+            URL[] urls = new URL[1];
+            URLStreamHandler streamHandler = null;
+            File classPath = new File(Constants.WEB_ROOT);
+
+            // 格式化url字符
+            String repository = (new URL("file", null, classPath.getCanonicalPath() + File.separator)).toString();
+
+            urls[0] = new URL(null, repository, streamHandler);
+            loader = new URLClassLoader(urls);
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+
+        Class myClass = null;
+        try {
+            myClass = loader.loadClass(servletName);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        }
+
+        Servlet servlet = null;
+        try {
+            servlet = (Servlet) myClass.newInstance();
+            servlet.service((ServletRequest) request, (ServletResponse) response);
+        } catch (Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (Throwable e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } ;
+    }
+}
