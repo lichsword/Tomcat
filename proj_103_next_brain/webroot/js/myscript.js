1491b48e47ed51616aff6be6f574e80a3736ffaa
简悦 <wangyue.wy@taobao.com>
2014-03-24 15:18:50 +0800

    "实现外部JS调用"

--- proj_103_next_brain/webroot/js/myscript.js
+++ proj_103_next_brain/webroot/js/myscript.js
@@ -1,4 +1,28 @@
-$("#btn_add").click(function(){
-    console.log("clicked.");
-    alert("outside js");
-})
+$(document).ready(function(){
+
+    $("#btn_refresh").click(function(){
+        $.get("/servlet/db",
+        function(data,status){
+            var element=document.getElementById("div1");
+            $(".div2").append(data);
+            console.log(data);
+            console.log(status);
+        });
+    });
+
+    $("#btn_get").click(function(){
+        console.log("clicked.");
+        alert("you clicked get button from out js.");
+    });
+
+    $("#btn_add").click(function(){
+        console.log("clicked.");
+        alert("you clicked add button");
+    });
+
+    $("#btn_delete").click(function(){
+        console.log("clicked.");
+        alert("you clicked delete button");
+    });
+
+});
