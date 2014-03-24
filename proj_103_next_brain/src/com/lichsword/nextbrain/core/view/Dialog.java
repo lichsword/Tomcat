df6ac72066d9661c21bc102e6633eded73e2ba68
简悦 <wangyue.wy@taobao.com>
2014-03-24 17:30:14 +0800

    "实现对话框的布局"

---
+++ proj_103_next_brain/src/com/lichsword/nextbrain/core/view/Dialog.java
@@ -0,0 +1,17 @@
+package com.lichsword.nextbrain.core.view;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lichsword
+ * Date: 14-3-24
+ * Time: 下午3:21
+ * <p/>
+ * TODO
+ */
+public class Dialog extends View{
+
+    @Override
+    public String html() {
+        return null;  // TODO
+    }
+}
