a1a7cc0a9fd2bc21f5b88798e1a36cf8928fb2ee
简悦 <wangyue.wy@taobao.com>
2014-04-13 21:19:44 +0800

    "完全美化css卡片模式"

--- proj_103_next_brain/src/com/lichsword/nextbrain/core/view/ListView.java
+++ proj_103_next_brain/src/com/lichsword/nextbrain/core/view/ListView.java
@@ -41,32 +41,43 @@ public class ListView<T> extends View {
             NBArticle article = (NBArticle) item;
             i++;
 //            sb.append(String.format("<p>序号：%d</p>", i));
-            sb.append(String.format("<span class=\"badge\">%d</span>", i));
-
-
+            sb.append(String.format("<span id=\"index\" class=\"badge\">%d</span>", i));
 //            String visitLevelString = (NBArticle.VL_PRIVAET == article.getVisitLevel() ? "私有" : "公开");
 //            sb.append(String.format("<p>权限：%s</p>", visitLevelString));
-
-
 //            String statusString = (NBArticle.ST_FIXED == article.getStatus() ? "已解决" : "未解决");
 //            sb.append(String.format("<p>状态：%s</p>", statusString));
+            sb.append(String.format("<p><b>问题：</b><span id=\"content-question\">%s</span></p>", article.getQuestion()));
+            sb.append(String.format("<p><b>描述：</b><span id=\"content-desc\">%s</span></p>", article.getDesc()));
+            sb.append(String.format("<p><b>标签：</b><span id=\"content-labels\">%s</span></p>", article.getLabels()));
+            sb.append(String.format("<p><b>本质：</b><span id=\"content-truth\">%s</span></p>", article.getTruth()));
+            sb.append(String.format("<p><b>模式：</b><span id=\"content-pattern\">%s</span></p>", article.getPattern()));
+            sb.append(String.format("<blockquote><h5 id=\"ref\">引用：%s</h5></blockquote>", article.getReference()));
+            sb.append(String.format("<p><b>示例：</b><span id=\"content-example\">%s</span></p>", article.getExample()));
+//            sb.append(String.format("<h5>创建时间：%s</h5>", article.getCreateTime()));
             sb.append("<div class=\"row\">");
-            sb.append(String.format("<div class=\"col-md-7\">问题：%s</div>", article.getQuestion()));
+            sb.append(String.format(
+                    "<div id=\"modified-time\" class=\"col-md-7\">%s</div>", article.getModifiedTime()));
+            int readCount = article.getReadCount();
+            String readCountString =
+                    (readCount > 10000)
+                            ? String.format("%.0f万", (float)readCount / 10000)
+                            : String.format("%d", readCount);
             sb.append(String.format(
                     "<div class=\"col-md-1\">"
-                    + "<span class=\"glyphicon glyphicon-eye-open\"> %d</span>"
-                    + "</div>", article.getReadCount()));
+                            + "<span class=\"glyphicon glyphicon-eye-open color-gray\"></span>"
+                            + "<span id=\"read-count\"> %s</span>"
+                            + "</div>", readCountString));
             int visitLevel = article.getVisitLevel();
             sb.append(String.format(
-                    "<div class=\"col-md-1\">"
-                    + "ID<span class=\"badge\">%d</span>"
-                    + "</div>", article.getId()));
+                    "<div id=\"article-id\" class=\"col-md-1\">"
+                            + "ID %d"
+                            + "</div>", article.getId()));
             if (NBArticle.VL_PRIVAET == visitLevel) {
                 // "私有"显示“锁”图标，否则不显示
                 sb.append(
-                    "<div class=\"col-md-1\">"
-                    + "<span class=\"glyphicon glyphicon-lock\"></span>"
-                    + "</div>"
+                        "<div class=\"col-md-1\">"
+                                + "<span id=\"visit\" class=\"glyphicon glyphicon-lock\"></span>"
+                                + "</div>"
                 );
             }// end if
 
@@ -74,21 +85,12 @@ public class ListView<T> extends View {
             if (NBArticle.ST_FIXED != status) {
                 // "未解决"则显示“锁”图标，否则不显示
                 sb.append(
-                    "<div class=\"col-md-1\">"
-                    + "<span class=\"glyphicon glyphicon-question-sign\"></span>"
-                    + "</div>"
+                        "<div class=\"col-md-1\">"
+                                + "<span id=\"status\"class=\"glyphicon glyphicon-question-sign\"></span>"
+                                + "</div>"
                 );
             }// end if
-
             sb.append("</div>");
-            sb.append(String.format("<h5>描述：%s</h5>", article.getDesc()));
-            sb.append(String.format("<p>标签：%s</p>", article.getLabels()));
-            sb.append(String.format("<p>本质：%s</p>", article.getTruth()));
-            sb.append(String.format("<p>模式：%s</p>", article.getPattern()));
-            sb.append(String.format("<blockquote><p>引用：%s</p></blockquote>", article.getReference()));
-            sb.append(String.format("<p>示例：%s</p>", article.getExample()));
-//            sb.append(String.format("<h5>创建时间：%s</h5>", article.getCreateTime()));
-            sb.append(String.format("<span class=\"badge\">%s</span>", article.getModifiedTime()));
             sb.append("<hr/>");
         }// end for
         sb.append("</div>");
